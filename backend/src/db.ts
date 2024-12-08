b04669257cb9cf428804acef718b6f41c04c5e9b
Seva Zaikov <seva.zaikov@gmail.com>
2024-12-08 00:59:16 -0800

    add basic fastify server setup

---
+++ backend/src/db.ts
@@ -0,0 +1,17 @@
+import mysql from "mysql2/promise";
+
+// Connection configuration
+const dbConfig: mysql.PoolOptions = {
+  host: process.env.DB_HOST,
+  user: process.env.user,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10,
+  idleTimeout: 60000,
+  queueLimit: 0,
+};
+
+// Create a connection pool
+const pool = mysql.createPool(dbConfig);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,35 @@
-console.log("Hello world!");
+import Fastify from "fastify";
+
+async function applyMigrations() {
+  // pass
+}
+
+async function startServer() {
+  const fastify = Fastify({ logger: true });
+
+  fastify.get("/", async function handler(request, reply) {
+    return { hello: "world" };
+  });
+
+  try {
+    await fastify.listen({
+      port: 3000,
+      /**
+       * Because we run this app in the Docker, we need to enable all connections.
+       * By default, Fastify only allows localhost/IPv6 connections.
+       * In Docker, as an isolated environment, that should be secure.
+       */
+      host: "0.0.0.0",
+    });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+
+async function main() {
+  await applyMigrations();
+  await startServer();
+}
+
+main();
