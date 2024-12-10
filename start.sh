0e0ba953c97c1b887562284d05b3fbfe1626c078
Seva Zaikov <seva.zaikov@gmail.com>
2024-12-10 04:44:07 -0800

    improve dev and test setups

--- start.sh
+++ start.sh
@@ -1,20 +1,52 @@
-# normal command to start dev mode (with reloading):
-docker compose -f compose.yml -f compose.dev.yml up
+#!/bin/bash
+
+start_local_server() {
+    # normal command to start dev mode (with reloading)
+    docker compose -f compose.yml -f compose.dev.yml up --build
+}
+
+start_integration_tests() {
+    docker compose -f compose.yml -f compose.test.yml up --build --exit-code-from test
+}
+
+stop_docker_containers() {
+    # To stop the containers while preserving them
+    docker compose stop
+}
+
+destroy_docker_containers() {
+    # To remove volumes (this will nuke database data)
+    docker compose down -v
+}
+
+if [ $# -eq 0 ]; then
+    start_local_server
+fi
+
+# Get the command from first argument
+command="$1"
+# Shift all arguments to the left (remove the first argument)
+shift
+
+case "$command" in
+    "test")
+        start_integration_tests
+        ;;
+    "stop")
+        stop_docker_containers
+        ;;
+    "destroy")
+        destroy_docker_containers
+        ;;
+    *)
+        echo "Unknown command: $command"
+        echo "Available commands: test, stop, destroy. No command will run dev server."
+        exit 1
+        ;;
+esac
 
 # production start (will compile TS to JS and only start with production dependencies):
 # docker compose -f compose.yml -f compose.prod.yml up -d
 
-# To stop the containers while preserving them (you can restart them later):
-# docker compose stop
-
-# To stop and remove the containers, networks, but preserve the volumes:
-# docker compose down
-
-# If you also want to remove the volumes (this will delete all your database data):
-# docker compose down -v
-
 # To connect to the database:
 # docker compose exec mysql mysql -u user -ppassword myapp
-
-# docker compose -f compose.yml -f compose.test.yml up --build --exit-code-from test
-# docker compose -f compose.yml -f compose.test.yml up --exit-code-from test
